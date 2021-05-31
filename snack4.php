e65a5a6487e5f4caa0270d948791c7e84a32d2a6
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-06-01 00:09:03 +0200

    snack 4 OK

--- snack4.php
+++ snack4.php
@@ -4,8 +4,8 @@ lo stesso numero più di una volta -->
 <?php
 $arrayRandom = [];
 $i = 0;
-while(count($arrayRandom) < 15) {
-    $numRand = rand(0, 10);
+while (count($arrayRandom) < 15) {
+    $numRand = rand(0, 100);
     if (!in_array($numRand, $arrayRandom)) {
         $arrayRandom[] = $numRand;
         $i++;
@@ -23,6 +23,16 @@ while(count($arrayRandom) < 15) {
 </head>
 
 <body>
+    <h1> ARRAY NUMERI RANDOM </h1>
+    <ul>
+        <?php 
+                for($i = 0; $i<count($arrayRandom);$i++){
+                    ?>
+                    <li><?php echo $arrayRandom[$i] ?></li>
+                <?php
+                }
+                ?>
+    </ul>
 </body>
 
 </html>
