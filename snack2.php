cf06f1a1c416f281598da3c90f3a2454bac6b727
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-06-01 11:23:19 +0200

    key exist fix error

--- snack2.php
+++ snack2.php
@@ -17,38 +17,39 @@ $isLogged = false;
 </head>
 
 <body>
-    <?php 
-        if(!$isLogged){
-            var_dump($isLogged);
-         ?>   
-         <form method="GET">
-        <label for="name"> NAME <input type="text" name="name" id="name"></label>
-        <label for="mail"> MAIL <input type="text" name="mail" id="mail"></label>
-        <label for="mail"> AGE <input type="number" name="age" id="age"></label>
-        <button type="submit"> INVIA </button>
-        <?php
-            $name = $_GET["name"];
-            $mail = $_GET["mail"];
-            $age = $_GET["age"];
-            $result = strlen($name)>3 && strpos($mail,"@") && strpos($mail,".");
-            var_dump($result);
-            if(strlen($name)>3 && strpos($mail,"@") && strpos($mail,".")){
-               $isLogged =true;
-               ?>
-                <h2>Accesso riuscito</h2>
-               <?php
-            }
-            else{
+    <?php
+    if (!$isLogged) {
+        var_dump($isLogged);
+    ?>
+        <form method="GET">
+            <label for="name"> NAME <input type="text" name="name" id="name"></label>
+            <label for="mail"> MAIL <input type="text" name="mail" id="mail"></label>
+            <label for="mail"> AGE <input type="number" name="age" id="age"></label>
+            <button type="submit"> INVIA </button>
+            <?php
+            if (key_exists("name", $_GET) && key_exists("mail", $_GET) && key_exists("age", $_GET)) {
+                $name = $_GET["name"];
+                $mail = $_GET["mail"];
+                $age = $_GET["age"];
+                $result = strlen($name) > 3 && strpos($mail, "@") && strpos($mail, ".");
+                var_dump($result);
+                if (strlen($name) > 3 && strpos($mail, "@") && strpos($mail, ".")) {
+                    $isLogged = true;
+            ?>
+                    <h2>Accesso riuscito</h2>
+                <?php
+                } else {
                 ?>
-                <h2>Accesso negato</h2>
-               <?php
+                    <h2>Accesso negato</h2>
+        <?php
+                }
             }
         }
         ?>
-        
 
 
-    </form>
+
+        </form>
 </body>
 
 </html>
